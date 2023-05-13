b55b3306bd78ed6829e9d18a998400985a1678cc
Vinayak B <117895974+VinayakB07@users.noreply.github.com>
2023-05-13 20:29:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(item => {
+  if(item.marks>50){
+    console.log(item.name);
+  	}
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(item => {
+    if(item.marks>50){
+      console.log(item.name);
+    	}
+    })
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:3, name:"karen", age:"19",marks:35});
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr.forEach(item => {
+        if(item.marks<50){
+          arr.splice((item.id-1),1);
+        	}
+        })
+        console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let Arr = [
+    { id: 4, name: "a", age: "18", marks: 78 },
+    { id: 5, name: "b", age: "20", marks: 55 },
+    { id: 6, name: "c", age: "19", marks: 30 },
+  ];
+   arr=arr.concat(Arr);
+   console.log(arr);
 }
